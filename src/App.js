db09717c5ee8008c84e218abff25c2d037d0d9b4
shreyansh-error-89 <shreyanshrathore75649@gmail.com>
2023-04-02 16:40:51 +0530

    first commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
+var data = require("./MOCK_DATA.json");
 
-function App() {
+export default function App() {
+  const [value, setValue] = useState("");
+  const [lastValue, setLastValue] = useState("");
+  const [pinnedTerms, setPinnedTerms] = useState([]);
+  const inputRef = useRef(null);
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onKeyDown = (event) => {
+    if (event.keyCode === 8 && value === "") {
+      if (lastValue !== "") {
+        setValue(lastValue);
+        setLastValue("");
+      }
+      event.preventDefault();
+    }
+  };
+
+  const onSearch = (searchTerm) => {
+    setValue(searchTerm);
+    setLastValue(searchTerm);
+    // our api to fetch the search result
+    console.log("search ", searchTerm);
+
+    // add the search term to pinned terms
+    setPinnedTerms((prevTerms) => [...prevTerms, searchTerm]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Search Country</h1>
+
+
+
+      <div className="all-container">
+
+        <div className="pinned-terms">
+          {pinnedTerms.map((term) => (
+            <div
+              key={term}
+              className="pinned-term"
+              onClick={() => {
+                setValue(term);
+                setLastValue(term);
+                inputRef.current.focus();
+              }}
+            >
+              <div className="btn-mod">
+
+              <span>{term}</span>
+              <button
+                className="btn-x"
+                onClick={() =>
+                  setPinnedTerms((prevTerms) =>
+                    prevTerms.filter((prevTerm) => prevTerm !== term)
+                  )
+                }
+              >
+                X 
+              </button>
+              </div>
+            </div>
+          ))}
+        </div>
+
+
+
+
+        <div className="search-container">
+          <div className="search-inner">
+            <input
+              type="text"
+              value={value}
+              onChange={onChange}
+              onKeyDown={onKeyDown}
+              ref={inputRef}
+            />
+            <button onClick={() => onSearch(value)}> Search </button>
+          </div>
+          <div className="dropdown">
+            {data
+              .filter((item) => {
+                const searchTerm = value.toLowerCase();
+                const fullName = item.Name.toLowerCase();
+                const code_name = item.Code.toLowerCase();
+
+                return (
+                  searchTerm &&
+                  fullName.startsWith(searchTerm) &&
+                  fullName !== searchTerm ||
+                  searchTerm === code_name
+                );
+              })
+              .slice(0, 10)
+              .map((item) => (
+                <div
+                  onClick={() => onSearch(item.Name)}
+                  className="dropdown-row"
+                  key={item.Name}
+                >
+                  {item.Name}
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 }
-
-export default App;
